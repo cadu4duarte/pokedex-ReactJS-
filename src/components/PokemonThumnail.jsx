5c505f0c6b39f633198cc6f96a1767e006b6f987
Carlos Eduardo <cadu4duarte@gmail.com>
2022-07-29 19:29:56 -0300

    feat: create pokemons thumb

---
+++ src/components/PokemonThumnail.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export const PokemonThumnail = ({id, name, image, type}) => {
+  const style = `thumb-container ${type}`
+
+  return(
+    <div className={style}>
+      <div className="number">
+        <small>#0{id}</small>
+
+        <img src={image} alt={name}/>
+        
+        <div className="detail-wrapper">
+          <h3>{name}</h3>
+          <small>Type: {type}</small>
+
+        </div>
+
+      </div>
+
+    </div>
+  );
+};
