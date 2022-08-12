680738b2655b83d04e7fec31a6e82004d18857a9
Carlos Eduardo <cadu4duarte@gmail.com>
2022-08-11 22:07:31 -0300

    style: edit layout

--- src/components/SideModal/SideModal.jsx
+++ src/components/SideModal/SideModal.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import Modal from 'react-modal';
+import "./style.css";
+import criar from "../../assets/criar.svg";
 
 export function SideModal({valueSideModal, setValueSideModal}) {
-  const customStyle = {
-    content: {
-      top: "50%",
-      left: "50%",
-      right: "auto",
-    }
-  }
-
   return (
     <Modal 
     isOpen={valueSideModal}
     onRequestClose={setValueSideModal}
-    style={customStyle}>
-      <h1>teste modal</h1>
+    className="sideModal">
+      <div className="modalHeader">
+        <img src={criar}/>
+        <h1>Criar card</h1>
+      </div>
+
+      <div className="modalContent">
+        <label>DIGITE UM NOME PARA O CARD</label>
+        <input type="text" placeholder="Digite o título"/>
+
+        <label>INCLUA UMA IMAGEM PARA APARECER NO CARD</label>
+        <input type="file" placeholder="Nenhum arquivo selecionado"/>
+      </div>
+
+      <div className="btnCreateBox"> 
+        <button >Criar card</button>
+      </div>
+      
+      
+      
     </Modal>
   )
 }
