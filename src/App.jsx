48ae5632b712a52d82619915f7289b044c5af67e
Carlos Eduardo <cadu4duarte@gmail.com>
2022-08-12 21:34:57 -0300

    feat: add use context import

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import {Home} from "./pages/Home";
 import {Header} from "./components/Header/Header";
 import Modal from "react-modal";
+import {AllPokemonProvider} from "./components/Context/AllPokemon";
+import {SinglePokemonProvider} from "./components/Context/SinglePokemon";
 
 Modal.setAppElement("#root");
 
 function App() {
 
   return (
-    <div className="App">
-      <Header/>
-      <Home/>
-    </div>
+    <SinglePokemonProvider>
+      <AllPokemonProvider>
+        <div className="App">
+          <Header/>
+          <Home/>
+        </div>
+      </AllPokemonProvider>
+    </SinglePokemonProvider>
+
+    
   )
 }
 
